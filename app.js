dfdc7501a26f7514d26a0863788381ed2acc4715
pirovo <154833505+pirovo@users.noreply.github.com>
2025-01-29 09:40:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const _ = require('underscore');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var animals = {
     "cat": "meow",
     "dog": "bark",
@@ -22,26 +22,40 @@ function getAnimal() {
 
 const app = express();
 
-app.get('/', function(req, res){
-  const [animal_name, sound] = getAnimal();
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`George Orwell had a farm.<br />
-E-I-E-I-O<br />
-And on his farm he had a ${ animal_name }.<br />
-E-I-E-I-O<br />
-With a ${ sound }-${ sound } here.<br />
-And a ${ sound }-${ sound } there.<br />
-Here a ${ sound }, there a ${ sound }.<br />
-Everywhere a ${ sound }-${ sound }.<br />`);
-      res.end();
+app.get('/', async (req, res, next) => {
+  try {
+    const [animal_name, sound] = getAnimal();
+    res.status(200).send(`
+      George Orwell had a farm.<br />
+      E-I-E-I-O<br />
+      And on his farm he had a ${animal_name}.<br />
+      E-I-E-I-O<br />
+      With a ${sound}-${sound} here.<br />
+      And a ${sound}-${sound} there.<br />
+      Here a ${sound}, there a ${sound}.<br />
+      Everywhere a ${sound}-${sound}.<br />
+    `);
+  } catch (error) {
+    next(error);
+  }
 });
-
-app.get('/api', function(req, res){
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify(animals));
-  res.end();
-})
-
-module.exports =  app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${ port }`)
+ 
+app.get('/api', async (req, res, next) => {
+  try {
+    res.status(200).json(animals);
+  } catch (error) {
+    next(error);
+  }
 });
+ 
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+ 
+const server = app.listen(port, () => {
+  console.log(`Launching server on http://localhost:${port}`);
+});
+ 
+module.exports = server; 
